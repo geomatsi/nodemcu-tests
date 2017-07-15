eb6cfd014b5e6e2bccb28eedcd48486a322dbbee
Sergey Matyukevich <geomatsi@gmail.com>
2017-07-15 22:48:33 +0300

    add simple command line tools to init.lua
    The tool names correlate with their Unix/Linux counterparts:
    - du: SPIFFS usage
    - free: RAM availability
    - ls: list files on SPIFFS and show their sizes
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- init.lua
+++ init.lua
@@ -1,14 +1,28 @@
---
+-- simple command line tools
+
 function ls()
 	for k,v in pairs(file.list()) do
-		print (k)
+		print (k, v .. ' bytes')
 	end
 end
 
--- init message
-print('MAC: ', wifi.sta.getmac())
-print('chip: ', node.chipid())
-print('heap: ', node.heap())
+function du()
+	f, u, t = file.fsinfo()
+	f = bit.rshift(f, 10)
+	u = bit.rshift(u, 10)
+	t = bit.rshift(t, 10)
+	print ('free: ', f .. ' kB')
+	print ('used: ', u .. ' kB')
+	print ('total: ', t .. ' kB')
+end
 
--- setup wifi
--- dofile("wifi.lua")
+function free()
+	print (bit.rshift(node.heap(), 10) .. ' kB')
+end
+
+-- init message
+
+print('Chip ID: ', node.chipid())
+print('Flash ID: ', node.flashid())
+print('Flash size (kB): ', bit.rshift(node.flashsize(), 10))
+print('Available heap (kB): ', bit.rshift(node.heap(), 10))
