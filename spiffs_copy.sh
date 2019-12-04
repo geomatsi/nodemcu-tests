d6c67fe655d60cf027d2ca74149c2c5ff6001ee9
Sergey Matyukevich <geomatsi@gmail.com>
2019-12-04 23:13:20 +0300

    update directory structure
    More simple examples to be added: need to do range test for wifi/mqtt.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- spiffs_copy.sh
+++ spiffs_copy.sh
@@ -4,13 +4,9 @@ TOOL=/home/matsi/bin/luatool.py
 PORT=/dev/ttyUSB0
 BAUD=115200
 
-LIST_INIT="init.lua"
-LIST_TEST="nrf24.lua test1.lua test2.lua test3.lua test4.lua"
+LIST ?= init.lua
 
 for f in ${LIST_INIT} ; do
 	sudo ${TOOL} --port ${PORT} --bar --baud ${BAUD} --src ${f} --dest ${f}
-done
-
-for f in ${LIST_TEST} ; do
-	sudo ${TOOL} --port ${PORT} --bar --compile --baud ${BAUD} --src ${f} --dest ${f}
+	#sudo ${TOOL} --port ${PORT} --bar --compile --baud ${BAUD} --src ${f} --dest ${f}
 done
