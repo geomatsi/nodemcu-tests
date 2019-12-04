c3c880169835a4d8d21067210ab0698a0fa8ad05
Sergey Matyukevich <geomatsi@gmail.com>
2019-12-04 23:22:10 +0300

    test: simple wifi test
    Slightly modified version from NodeMCU docs:
      - https://nodemcu.readthedocs.io/en/master/upload/#initlua
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- spiffs_copy.sh
+++ spiffs_copy.sh
@@ -4,9 +4,8 @@ TOOL=/home/matsi/bin/luatool.py
 PORT=/dev/ttyUSB0
 BAUD=115200
 
-LIST ?= init.lua
-
-for f in ${LIST_INIT} ; do
-	sudo ${TOOL} --port ${PORT} --bar --baud ${BAUD} --src ${f} --dest ${f}
+for f in "$@"
+do
+	sudo ${TOOL} --port ${PORT} --baud ${BAUD} --src ${f} --dest ${f}
 	#sudo ${TOOL} --port ${PORT} --bar --compile --baud ${BAUD} --src ${f} --dest ${f}
 done
---
+++ wifi/test1.lua
@@ -0,0 +1,47 @@
+dofile("cred.lua")
+
+wifi_connect_event = function(T)
+  print("Connection to AP("..T.SSID..") established!")
+  print("Waiting for IP address...")
+  if disconnect_ct ~= nil then disconnect_ct = nil end
+end
+
+wifi_got_ip_event = function(T)
+  print("Wifi connection is ready! IP address is: "..T.IP)
+end
+
+wifi_disconnect_event = function(T)
+  if T.reason == wifi.eventmon.reason.ASSOC_LEAVE then
+    return
+  end
+  local total_tries = 120
+  print("\nWiFi connection to AP("..T.SSID..") has failed!")
+
+  for key,val in pairs(wifi.eventmon.reason) do
+    if val == T.reason then
+      print("Disconnect reason: "..val.."("..key..")")
+      break
+    end
+  end
+
+  if disconnect_ct == nil then
+    disconnect_ct = 1
+  else
+    disconnect_ct = disconnect_ct + 1
+  end
+  if disconnect_ct < total_tries then
+    print("Retrying connection...(attempt "..(disconnect_ct+1).." of "..total_tries..")")
+  else
+    wifi.sta.disconnect()
+    print("Aborting connection to AP!")
+    disconnect_ct = nil
+  end
+end
+
+wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifi_connect_event)
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_got_ip_event)
+wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, wifi_disconnect_event)
+
+print("Connecting to WiFi access point...")
+wifi.setmode(wifi.STATION)
+wifi.sta.config({ssid=SSID, pwd=PASSWORD})
